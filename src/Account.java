792720abb20186e8b281c6ae6b9a37b944aecc10
Dreamerc1 <chelsreid99@gmail.com>
2024-07-22 16:45:18 +0100

    Adding class constructor

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Account {
+    //Class Variables
+    private int balance;
+    private int previousTransaction;
+    private String customerName;
+    private String customerID;
+
+    //Class constructor
+    Account(String cname, String cid){
+        customerName = cname;
+        customerID = cid;
+    }
+
+
+
+    
+    
+}
