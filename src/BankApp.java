4dcff3d75f00c9bec9fb25911d03c499e78adfe5
Dreamerc1 <chelsreid99@gmail.com>
2024-07-29 10:53:03 +0100

    Changing the name of the main class

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,4 +1,4 @@
-public class App {
+public class BankApp {
     public static void main(String[] args) throws Exception {
         Account chelsea = new Account ("Chelsea","001");
         chelsea.showMenu();
